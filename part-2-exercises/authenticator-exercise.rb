7c0aaf8d8b91dd709472b1b872042b0736ec5c41
Patrick Oswald <poswald338@gmail.com>
2022-02-21 11:04:43 -0600

    authenticator exercise

---
+++ part-2-exercises/authenticator-exercise.rb
@@ -0,0 +1,39 @@
+USERS = [
+  {username: "Patrick", password: 'password'},
+  {username: "John", password: 'password1'},
+  {username: "Tony", password: 'password2'},
+  {username: "Rick", password: 'password3'},
+  {username: "Aaron", password: 'password4'}
+]
+
+puts "Welcome to the Authenticator"
+25.times { print '-' }
+puts
+puts 'This program will take input from the User and compare password.'
+puts 'If correct, the application will return the user object.'
+
+def authenticateUser(un, pw)
+  USERS.each do |user|
+    if user[:username] === un && user[:password] === pw
+      return user
+    elsif user[:username] === un && user[:password] != pw
+      return 'Invalid password'
+    end
+  end
+  return 'Credentials were not correct'
+end
+
+attempt = 3
+while attempt > 0 do
+  print 'Username: '
+  username = gets.chomp
+  print 'Password: '
+  pw = gets.chomp
+  puts authenticateUser(username, pw)
+  puts 'Press n to quit or any other key to continue'
+  userInput =  gets.chomp.downcase
+  break if userInput == 'n'
+  attempt -= 1
+  puts "Attepmts left: #{attempt}"
+end
+puts 'You have exceeded the maximum number of attempts.' if attempt === 0
