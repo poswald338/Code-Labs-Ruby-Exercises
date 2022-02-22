ad5490d5696d2ec9aac06ef0e90e95de8b24cc0f
Patrick Oswald <poswald338@gmail.com>
2022-02-22 06:04:57 -0600

    updated exercise 2.9

--- part-2-exercises/exercise-2.9.rb
+++ part-2-exercises/exercise-2.9.rb
@@ -1,38 +1,32 @@
-ROMAN_TO_INT =
-  {
-    i: 1,
-    v: 5,
-    x: 10,
-    l: 50,
-    c: 100,
-    d: 500,
-    m: 1000
-  }
+ROMAN_VALS = {
+  i: 1,
+  v: 5,
+  x: 10,
+  l: 50,
+  c: 100,
+  d: 500,
+  m: 1000
+}
+loop_var = ''
+while loop_var != 'exit'
 
+  puts "Type in a roman numeral"
+  input = gets.chomp.downcase
 
-def roman_to_int(roman)
-  array = []
-  val_map = roman.split('').map { |el| el.downcase.to_sym}
-  val_map.map.with_index do |letter, index|
-    array.push(ROMAN_TO_INT[letter])
-    if val_map[]
+  val = input.split('').map { |n| n.to_sym }
+  calc = 0
+  val.each_with_index do |num, idx|
+    if (idx +1) < val.length && ROMAN_VALS[num] < ROMAN_VALS[val[idx + 1]] 
+      calc -= ROMAN_VALS[num]
+    else
+      calc += ROMAN_VALS[num]
+    end
   end
-  array
+  puts
+  puts "#{input.upcase} >>> #{calc}"
+  25.times {print'-'} 
+  puts
+  puts "enter exit to quit or anything else to continue"
+
+  loop_var = gets.chomp
 end
-
-
-# def roman_to_int roman
-#   value_map = roman.split('').map { |e| ROMAN_TO_INT[e.downcase.to_sym] }
-#   value_map.map.with_index do |e, idx| 
-#     unless value_map[idx + 1].nil?
-#     then
-#       value_map[idx + 1] > e ? -e : e
-#     else e
-#     end
-#   end.sum
-# end
-
-roman = "MDCCLXXVI"
-puts roman_to_int roman
-
-
