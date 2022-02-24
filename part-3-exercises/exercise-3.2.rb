be0f45c585aab1a9b1f931e56ea4b23ef63a2c47
Patrick Oswald <poswald338@gmail.com>
2022-02-24 12:50:02 -0600

    exercise 3.2

---
+++ part-3-exercises/exercise-3.2.rb
@@ -0,0 +1,28 @@
+class Animal
+  attr_accessor :name, :animal_type, :color 
+
+  @name
+  @animal_type
+  @color
+
+  def to_s
+    puts "My name is #{@name}"
+    puts "I am a #{@animal_type}"
+    puts "My fur color is #{@color}"
+  end
+
+  def self.intro(name, type, color)
+    "Hello. My name is #{name}, I am a #{type}, and my fur is #{color}"
+  end
+end
+
+dog = Animal.new
+dog.name = "Clifford"
+dog.animal_type = "dog"
+dog.color = "red"
+
+puts "My name is #{dog.name}"
+puts "I am a #{dog.animal_type}"
+puts "My fur color is #{dog.color}"
+
+puts Animal.intro(dog.name, dog.animal_type, dog.color)
