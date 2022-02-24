24b4d9dc7b43f71d9d65e5271d0edd4d653cda9f
Patrick Oswald <poswald338@gmail.com>
2022-02-24 16:12:37 -0600

    exercise 3.5

---
+++ part-3-exercises/exercise-3.5.rb
@@ -0,0 +1,52 @@
+class Person
+  attr_accessor :weight, :height, :name
+  @weight
+  @height
+  @name
+
+  def initialize(weight, height, name)
+    @weight = weight
+    @height = height
+    @name = name
+  end
+
+  def eat(food)
+    puts "Eating #{food}"
+  end
+end
+
+class Doctor < Person
+  def initialize(weight, height, name)
+    @weight = weight
+    @height = height
+    @name = name
+    puts "I am a #{self.class}!"
+  end
+end
+
+class Teacher < Person
+  def initialize(weight, height, name)
+    @weight = weight
+    @height = height
+    @name = name
+    puts "I am a #{self.class}!"
+  end
+end
+
+class Engineer < Person
+  def initialize(weight, height, name)
+    @weight = weight
+    @height = height
+    @name = name
+    puts "I am a #{self.class}!"
+  end
+end
+
+d = Doctor.new(120, "12'0", "Steven")
+d.eat("pizza")
+
+t = Teacher.new(150, "5'4", "Stacy")
+puts t.height
+
+e = Engineer.new(185, "6'0", "David")
+puts e.name, e.weight
